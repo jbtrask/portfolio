61c901ce68874b875de66cfe208987a704f6c5d4
James Trask <jbt_5@yahoo.com>
2011-07-15 12:29:50 -0700

    messing around with touch events

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -1,14 +1,27 @@
 
 $$(document).ready(function() {
 
-	$$(window).resize(setSize());
+	window.addEventListener('touchstart', touchStart, false);
+	window.addEventListener('touchend', touchEnd, false);
+	
+/*	$$(window).resize(setSize());
 	setSize();
 	
 	$$(window).swipe(function() { alert('swipe'); });
 	//$$(window).scrollstop(function() { alert('scroll'); });
 	$$(window).orientationchange(function() { alert('orientation'); });	
-	
+*/
 });
+
+var touchStartEvent;
+
+function touchStart(event) {
+	touchStartEvent = event;
+}
+
+function touchEnd(event) {
+	alert("start (" + touchStartEvent.touches[0].screenX + ", " + touchStartEvent.touches[0].screenY + ") - end (" + event.touches[0].screenX + ", " + event.touches[0].screenY + ")";
+}
 
 function setSize() {
 	console.log('setSize');
